b2bbb68cfacbe543ee56684bb7ac34984e2d2088
Niraj <158603545+NirazB@users.noreply.github.com>
2025-01-14 17:50:30 +0545

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'your-email'  # Website email , from this i can send
 app.config['MAIL_PASSWORD'] = 'your-app-password'  # Website email app password
-ADMIN_EMAIL = 'nirajbista25@gmail.com'  # Admin receiving verification emails
+ADMIN_EMAIL = 'admin-email'  # Admin receiving verification emails
 
 db = SQLAlchemy(app)
 mail = Mail(app)
